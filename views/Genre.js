94ceac0ca15abd7cf63a07003186c031b067cc58
Emil Schultz <emilrschultz@gmail.com>
2021-04-16 09:25:25 +0200

    Alert function when book is added ti My Books

--- views/Genre.js
+++ views/Genre.js
@@ -6,6 +6,7 @@ import {
   ScrollView,
   TouchableOpacity,
   StyleSheet,
+  Alert,
 } from "react-native";
 
 import { useMyBooks } from "../context/MyBooksContext";
@@ -13,17 +14,30 @@ import { useMyBooks } from "../context/MyBooksContext";
 const Genre = ({ route }) => {
   const genre = route.params.genre;
   const [books, setBooks] = useState([]);
-  // const [myBooks, setMyBooks] = useState([]);
+
+  // from MyBooksContext
   const adddNew = useMyBooks();
 
+  // show a lame alert pop up when added to My Books
+  const showAlert = () => {
+    Alert.alert("Added to My Books", "nice", [
+      {
+        text: "OK",
+        onPress: () => console.log("PRESSED: OK"),
+      },
+    ]);
+  };
+
+  // fetch books from clicked genre
   useEffect(() => {
     fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=${genre}&printType=books&orderBy=newest&maxResults=6`
+      `https://www.googleapis.com/books/v1/volumes?q=${genre}&printType=books&orderBy=newest&maxResults=10`
     )
       .then((response) => response.json())
       .then((data) => setBooks(data.items));
   }, []);
 
+  // map through fetched books and return desired parameters
   const GenreBooks = books.map((book) => {
     return (
       <View style={styles.container} key={book.id}>
@@ -37,14 +51,18 @@ const Genre = ({ route }) => {
           <Text>{book.volumeInfo.title}</Text>
           <Text>By {book.volumeInfo.authors}</Text>
           <Text>{book.volumeInfo.description}</Text>
+          <Text>Pages: {book.volumeInfo.pageCount}</Text>
         </View>
+        {/* add desired parameters to myBooksContext */}
         <TouchableOpacity
           onPress={() => {
             adddNew.addToMyBooks({
               title: book.volumeInfo.title,
               author: book.volumeInfo.authors,
               description: book.volumeInfo.description,
+              image: book.volumeInfo.imageLinks.smallThumbnail,
             });
+            showAlert();
           }}
         >
           <Text style={{ borderWidth: 0.5, backgroundColor: "white" }}>
